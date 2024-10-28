e226be13e1de92ed1e108518925d7e1e13b0ee60
Giovanni Grano <me@giograno.com>
2024-10-28 14:51:47 +0300

    Update update_latest.py

--- bin/update_latest.py
+++ bin/update_latest.py
@@ -4,13 +4,21 @@ from pathlib import Path
 import yaml
 
 from localstack.utils.openapi import get_localstack_openapi_spec
+from localstack.version import version as ls_version
 
-openapi_path = Path(os.path.dirname(__file__)) / ".." / "openapi" / "emulators"/ "localstack-spec-latest.yml"
+import click
 
-
-def main():
+@click.command()
+@click.option('--latest', is_flag=True, default=False, help='If enabled, sets the version of the spec to latest.')
+def update_aws_spec(latest: bool) -> None:
+    """
+    This function retrieves the OpenAPI specs declared via plugins, sets a given version and store the result
+    in a file at a given path.
+    """
+    version = "latest" if latest else ls_version
+    openapi_path = Path(os.path.dirname(__file__)) / ".." / "openapi" / "emulators"/ f"localstack-spec-{version}.yml"
     spec = get_localstack_openapi_spec()
-    spec["info"]["version"] = "latest"
+    spec["info"]["version"] = version
     if not openapi_path.parent.exists():
         openapi_path.parent.mkdir()
     with open(openapi_path, "w") as f:
@@ -18,5 +26,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
+    update_aws_spec()
